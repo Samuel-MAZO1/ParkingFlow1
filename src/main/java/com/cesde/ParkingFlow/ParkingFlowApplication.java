25c9906810a4d21e616e9ea99c844e1f796cbe10
Samuel <smazo870@gmail.com>
2026-03-10 13:22:30 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/cesde/ParkingFlow/ParkingFlowApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.ParkingFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesde/ParkingFlow/ParkingFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.ParkingFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
